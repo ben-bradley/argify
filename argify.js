c480442d47e2e2322ac3d7705316c28f0772e735
Ben Bradley <ben@bradleyit.com>
2014-05-28 20:55:16 -0700

    initial drop

---
+++ argify.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+  var args = {};
+  process.argv.forEach(function(arg) {
+    arg = arg.match(/^--([^\=]+)=(.+)/);
+    if (arg)
+      args[arg[1]] = arg[2];
+  })
+  return args;
+};
